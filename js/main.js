edc21a372a6cc46182da1ca12cac3ddd98242f96
HarshanaBandaraAg91 <139755295+HarshanaBandaraAg91@users.noreply.github.com>
2023-12-14 19:42:27 +0530

    make navbar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+     
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}      
+
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== DAY COUNTER FOR CHRISTMAS ===============*/
 

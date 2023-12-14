ce841d1d28f0ea530f5c51d222dc7d3c59a717b0
HarshanaBandaraAg91 <139755295+HarshanaBandaraAg91@users.noreply.github.com>
2023-12-14 23:00:52 +0530

    add countdown

--- js/main.js
+++ js/main.js
@@ -28,4 +28,73 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== DAY COUNTER FOR CHRISTMAS ===============*/
+const titleData = documnet.getElementById('title-data'),
+      numberData = document.getElementById('number-data'),
+      textData = document.getElementById('text-data'),
+      msgChristmas = document.getElementById('msg-christmas')
 
+const christmasCountdown = () => {
+    let now = new Date(),
+    currentMonth = now.getMonth() + 1,
+    currentDay = now.getDate()
+
+
+    let nextChristmasYear = now.getFullYear()
+    if (currentMonth == 12 && currentDay > 25) {
+        nextChristmasYear +=1
+    }
+
+    let nextChristmasDate = 'Dec 25, ${nextChristmasYear} 00:00:00',
+        christmasDay = new Date{nextChristmasDate},
+        timeLeft = christmasDay - now
+
+    let days = 0,
+        hours = 0,
+        minutes = 0,
+        seconds = 0
+
+
+    if (currentMonth != 12 || (currentMonth == 12 && currentDay != 25)) {
+        days = Math.floor(timeLeft / 1000 / 60 / 60 / 24)
+        hours = Math.floor(timeLeft / 1000 / 60 / 60) % 24
+        minutes = Math.floor(timeLeft / 1000 / 60) % 60
+        seconds = Math.floor(timeLeft / 1000) % 60
+    }  
+    
+    
+    numberData.innerHTML = days < 10 ? '0${days}' : days
+    textData.innerHTML = 'Days'
+
+
+    if (currentDay == 24) {
+        numberData.innerHTML = hours < 10 ? '0${hours}' : hours
+        textData.innerHTML = 'Hours'
+    }
+
+
+    if (currentDay == 24 && hours === 0) {
+        numberData.innerHTML = minutes < 10 ? '0${minutes}' : minutes
+        textData.innerHTML = 'Minutes'
+    }
+
+
+    if (currentDay == 24 && hours === 0 && minutes === 0) {
+        numberData.innerHTML = seconds < 10 ? '0${seconds}' : seconds
+        textData.innerHTML = 'Seconds'
+    }
+
+
+    if (currentMonth == 12 && currentDay == 25) {
+        titleData.style.display = 'none'
+        msgChristmas.style.display = 'block'
+        msgChristmas.style.innerHTML = 'Today is Dec 25, Merry Christmas'
+    }
+
+
+    if (currentMonth == 12 && currentDay == 26) {
+        titleData.style.display = 'block'
+        msgChristmas.style.display = 'none'
+    }
+}      
+
+setInterval(christmasCountdown, 1000)
